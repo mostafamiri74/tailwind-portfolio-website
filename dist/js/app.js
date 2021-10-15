5cd6337af9c969d81f921a7310342738e64cbc01
mostafamiri74 <mostafamiri12345@gmail.com>
2021-10-15 16:03:11 +0330

    add information & connection & footer section

--- dist/js/app.js
+++ dist/js/app.js
@@ -62,3 +62,12 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+var map = L.map("map").setView([35.730428, 51.3383063], 14);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map).openPopup();
